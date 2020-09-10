019c864d9478f3b571430e92501ee3d2577cb932
yarynakorduba <yaryna.korduba@keenethics.com>
2020-09-10 23:25:34 +0300

    Updates

---
+++ precache-manifest.8aaa32a9634ffe10da7aa188cd00b414.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33d99f959b262492df92bdfff2a488e4",
+    "url": "/ram-viz/index.html"
+  },
+  {
+    "revision": "2de8f9a862eefa4a3756",
+    "url": "/ram-viz/static/css/main.7177b98f.chunk.css"
+  },
+  {
+    "revision": "6a81f11490af3756e84c",
+    "url": "/ram-viz/static/js/2.39caa0ed.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ram-viz/static/js/2.39caa0ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2de8f9a862eefa4a3756",
+    "url": "/ram-viz/static/js/main.298f41b9.chunk.js"
+  },
+  {
+    "revision": "efb82134989a71f5c67c",
+    "url": "/ram-viz/static/js/runtime-main.77b5c6db.js"
+  }
+]);
